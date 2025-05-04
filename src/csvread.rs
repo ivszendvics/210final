5d904b21cc541631dfc9be26e2a53a686c269eed
ivszendvics <ivanwiandt@gmail.com>
2025-05-03 23:47:20 -0400

    Update csvread.rs
    made fields public

--- src/csvread.rs
+++ src/csvread.rs
@@ -8,24 +8,24 @@ use std::fmt;
 use std::process;
 
 pub struct Student {
-    id: i32,
-    gender: String,
-    age: f32,
-    city: String,
-    profession: String,
-    academic_pressure: f32,
-    work_pressure: f32, //I think all the values for this are 0? but the kaggle says it's in a range from 0-.25 so im setting it as a f32 just in case
-    cgpa: f32,
-    study_satisfaction: f32,
-    job_satisfaction: f32,
-    sleep_duration: f32, //just changing this to a f32 value instead of a string
-    dietary_habits: f32, //changing this too
-    degree: String,
-    suicidal_thoughts: f32,
-    workstudy_hours: f32,
-    financial_stress: f32,
-    family_history: f32,
-    depression: f32,
+    pub id: i32,
+    pub gender: String,
+    pub age: f32,
+    pub city: String,
+    pub profession: String,
+    pub academic_pressure: f32,
+    pub work_pressure: f32, //I think all the values for this are 0? but the kaggle says it's in a range from 0-.25 so im setting it as a f32 just in case
+    pub cgpa: f32,
+    pub study_satisfaction: f32,
+    pub job_satisfaction: f32,
+    pub sleep_duration: f32, //just changing this to a f32 value instead of a string
+    pub dietary_habits: f32, //changing this too
+    pub degree: String,
+    pub suicidal_thoughts: f32,
+    pub workstudy_hours: f32,
+    pub financial_stress: f32,
+    pub family_history: f32,
+    pub depression: f32,
 }
 
 pub fn parse_sleep_duration(s: &str) -> f32 {
@@ -80,10 +80,10 @@ pub fn csvread(path: &str) -> Result<Vec<Student>, Box<dyn Error>> {
 
         let student = Student{
             id: r[0].parse().unwrap_or(0),
-            gender: r[1].to_string(),
+            gender: r[1].to_string(), //not using this
             age: r[2].parse().unwrap_or(0.0),
-            city: r[3].to_string(),
-            profession: r[4].to_string(),
+            city: r[3].to_string(), //not using this either
+            profession: r[4].to_string(), //or this
             academic_pressure: r[5].parse().unwrap_or(0.0),
             work_pressure: r[6].parse().unwrap_or(0.0),
             cgpa: r[7].parse().unwrap_or(0.0),
@@ -91,7 +91,7 @@ pub fn csvread(path: &str) -> Result<Vec<Student>, Box<dyn Error>> {
             job_satisfaction: r[9].parse().unwrap_or(0.0),
             sleep_duration: parse_sleep_duration(&r[10]),
             dietary_habits: parse_dietary_habits(&r[11]),
-            degree: r[12].to_string(),
+            degree: r[12].to_string(), //or this
             suicidal_thoughts: parse_suicidethoughts(&r[13]),
             workstudy_hours: r[14].parse().unwrap_or(0.0),
             financial_stress: r[15].parse().unwrap_or(0.0),
